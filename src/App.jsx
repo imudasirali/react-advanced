71389b521666f4c0c83ce2fd5c01bc1b43d0c0d7
Mudasir Ali <mudasir.ali@axelerant.com>
2024-11-05 19:16:18 +0530

    LDT-1138: modal exercise project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
-function App() {
+import { useState } from "react";
+import { CustomModal } from "./components/CustomModal";
+import { DialogModal } from "./components/DialogModal";
+import "./styles.css";
+
+export default function App() {
+  const [isCustomModalOpen, setIsCustomModalOpen] = useState(false);
+  const [isDialogModalOpen, setIsDialogModalOpen] = useState(false);
 
   return (
-    <h1>Hi</h1>
-  )
-}
+    <div>
+      <button onClick={() => setIsCustomModalOpen(true)}>
+        Show Custom Modal
+      </button>
+      <br />
+      <button onClick={() => setIsDialogModalOpen(true)}>
+        Show Dialog Modal
+      </button>
+      <CustomModal
+        isOpen={isCustomModalOpen}
+        onClose={() => setIsCustomModalOpen(false)}
+      >
+        <p>
+          This is a <strong>CUSTOM</strong> modal
+        </p>
+        <button onClick={() => setIsCustomModalOpen(false)}>Close</button>
+      </CustomModal>
 
-export default App
+      <DialogModal
+        isOpen={isDialogModalOpen}
+        onClose={() => setIsDialogModalOpen(false)}
+      >
+        <p>
+          This is a <strong>DIALOG</strong> modal
+        </p>
+        <button onClick={() => setIsDialogModalOpen(false)}>Close</button>
+      </DialogModal>
+    </div>
+  );
+}
