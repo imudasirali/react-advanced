5f0911b7ad0e8952c9c8137b010f13716fc5fd4e
Mudasir Ali <mudasir.ali@axelerant.com>
2024-11-06 10:43:51 +0530

    LDT-1138: error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-function App() {
+import { ErrorBoundary } from "./ErrorBoundary";
 
+function App() {
   return (
-    <h1>Hi</h1>
-  )
+    <>
+      <h1>This is the parent</h1>
+      <ErrorBoundary fallback={<h2>Error in the Component</h2>}>
+        <Child />
+      </ErrorBoundary>
+    </>
+  );
 }
 
-export default App
+export default App;
+
+function Child() {
+  return <h3>This is a child</h3>;
+}
---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+export class ErrorBoundary extends React.Component {
+  state = { hasError: false };
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+    return this.props.children;
+  }
+}
